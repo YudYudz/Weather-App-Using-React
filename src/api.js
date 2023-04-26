f0bd048ec98b2635616506b34d70c543b53830be
YudYudz <arthadiaz1@gmail.com>
2023-04-27 03:36:56 +0700

    all done

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'aa45cf1e74mshe74314899b12a47p127c6ajsn0f64752d5c0a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ece5012ffa6b153b9b445c1e535df2b3";
